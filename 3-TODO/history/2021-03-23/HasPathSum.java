af288f8b1dd8618d5f01415f38fceec31073375b
y-rui <965486976@qq.com>
2022-04-17 21:21:13 +0800

    renameSomething

---
+++ 3-TODO/history/2021-03-23/HasPathSum.java
@@ -0,0 +1,66 @@
+/* 
+日期：2021-03-23
+112. 路径总和
+*/
+
+/*
+算法思想：广度优先搜索
+时间复杂度：O(n)
+空间复杂度：O(n)
+*/
+
+class Solution1 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queNode = new LinkedList<TreeNode>();
+        Queue<Integer> queVal = new LinkedList<Integer>();
+        queNode.offer(root);
+        queVal.offer(root.val);
+        while (!queNode.isEmpty()) {
+            TreeNode now = queNode.poll();
+            int temp = queVal.poll();
+            if (now.left == null && now.right == null) {
+                if (temp == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (now.left != null) {
+                queNode.offer(now.left);
+                queVal.offer(now.left.val + temp);
+            }
+            if (now.right != null) {
+                queNode.offer(now.right);
+                queVal.offer(now.right.val + temp);
+            }
+        }
+        return false;
+    }
+}
+
+/*
+算法思想：递归
+时间复杂度：O(n)
+空间复杂度：O(h)(h为树的高度)
+*/
+
+class Solution2 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+
+
+
+
+
+
+
---
+++ 3-TODO/history/2021-03-29/LengthOfLIS.java
@@ -0,0 +1,75 @@
+/* 
+日期：2021-03-29
+300. 最长递增子序列
+*/
+
+/*
+算法思想：动态规划
+时间复杂度：O(n2)
+空间复杂度：O(n)
+*/
+
+class Solution1 {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
+
+/*
+算法思想：贪心 + 二分查找
+时间复杂度：O(nlogn)
+空间复杂度：O(n)
+*/
+
+class Solution2 {
+    public int lengthOfLIS(int[] nums) {
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; 
+                // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+}
+
+
+
+
+
+
+
+
